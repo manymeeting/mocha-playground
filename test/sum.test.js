873ff0105438f07e8b77ebb92319d6cb4f583f4e
Mokuten OH <mokuteno@gmail.com>
2018-01-27 22:47:34 -0800

    add more test cases for sum function

--- test/sum.test.js
+++ test/sum.test.js
@@ -3,13 +3,15 @@ import sum from '../sum.js';
 import chai from '../chai.js';
 import mocha from '../mocha.js';
 
-// mocha.setup('bdd');
-// mocha.checkLeaks();
-// mocha.run();
-
-describe('sum', function () {
-  it('should return sum of arguments', function () {
+describe('Test Sum', function () {
+  it('should return sum of arguments 1', function () {
     chai.expect(sum(1, 2)).to.equal(3);
   });
+
+  it('should return sum of arguments 2', function () {
+    chai.expect(sum(2, 3)).to.equal(5);
+  });
+
+  
 });
 
