c64bfdf6f99ef0127cd31278e91884530ec82aa0
Mokuten OH <mokuteno@gmail.com>
2018-01-28 23:53:53 -0800

    add test cases for constrain()

--- test/util.test.js
+++ test/util.test.js
@@ -1,22 +1,41 @@
 
 import {transformStringToHex} from '../util.js';
+import {constrain} from '../util.js';
 import chai from '../chai.js';
 import mocha from '../mocha.js';
 
 describe('Test transformStringToHex', function () {
 
-  it('should return HEX for str 1', function () {
-    chai.expect(transformStringToHex("#123456")).to.equal(parseInt( "123456", 16 ));
-  });
+	it('should return HEX for str 1', function () {
+		chai.expect(transformStringToHex("#123456")).to.equal(parseInt( "123456", 16 ));
+	});
 
-  it('should return null for empty string', function () {
-    chai.expect(transformStringToHex("")).to.equal(null);
-  });
+	it('should return null for empty string', function () {
+		chai.expect(transformStringToHex("")).to.equal(null);
+	});
 
-  it('should return null for invalid string', function () {
-    chai.expect(transformStringToHex("123456")).to.equal(null);
-  });
+	it('should return null for invalid string', function () {
+		chai.expect(transformStringToHex("123456")).to.equal(null);
+	});
 
+});
+
+describe('Test constrain', function () {
+
+	var min = 10;
+	var max = 100;
+
+	it('should return min for input < min', function () {
+		chai.expect(constrain(9, min, max)).to.equal(min);
+	});
+
+	it('should return max for input > max', function () {
+		chai.expect(constrain(101, min, max)).to.equal(max);
+	});
+
+	it('should return value for min < value < max', function () {
+		chai.expect(constrain(11, min, max)).to.equal(11);
+	});
   
 });
 
--- util.js
+++ util.js
@@ -9,3 +9,19 @@ export function transformStringToHex( str ) {
     return parseInt( str.substring( 1 ), 16 );
 
 }
+
+export function constrain ( v, min, max ) {
+
+    if ( v < min ) {
+
+        v = min;
+
+    } else if ( v > max ) {
+
+        v = max;
+
+    }
+
+    return v;
+
+}
